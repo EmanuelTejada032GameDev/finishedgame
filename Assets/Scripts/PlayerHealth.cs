b81d25e5f16c90639857228ed8b63ba37c59b893
Emanuel <emanueltejada311295@gmail.com>
2021-06-09 14:07:48 -0400

    Initial commit

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+	public int totalHealth = 3;
+	public RectTransform heartUI;
+
+	//Game over menu
+	public RectTransform gameOverMenu;
+	public GameObject hordes;
+	
+	private int health;
+	private float heartSize = 16f;
+
+	private SpriteRenderer _renderer;
+	private Animator _animator;
+	private PlayerController _playerController;
+
+
+	private void Awake()
+	{
+		_renderer = GetComponent<SpriteRenderer>();
+		_animator = GetComponent<Animator>();
+		_playerController = GetComponent<PlayerController>();
+	}
+
+	void Start()
+    {
+		health = totalHealth;
+    }
+
+	public void AddDamage(int amount)
+	{
+		health = health - amount;
+
+		// Visual Feedback
+		StartCoroutine("VisualFeedback");
+
+		// Game  Over
+		if (health <= 0) {
+			health = 0;
+			gameObject.SetActive(false);
+
+		}
+
+		heartUI.sizeDelta = new Vector2(health * heartSize, heartSize);
+		Debug.Log("Player got damaged. His current health is " + health);
+	}
+
+	public void AddHealth(int amount)
+	{
+		health = health + amount;
+
+		// Max health
+		if (health > totalHealth) {
+			health = totalHealth;
+		}
+		heartUI.sizeDelta = new Vector2(health * heartSize, heartSize);
+		Debug.Log("Player got some life. His current health is " + health);
+	}
+
+	private IEnumerator VisualFeedback()
+	{
+		_renderer.color = Color.red;
+
+		yield return new WaitForSeconds(0.1f);
+
+		_renderer.color = Color.white;
+	}
+
+    private void OnEnable()
+    {
+		health = totalHealth;
+    }
+
+    private void OnDisable()
+    {
+		gameOverMenu.gameObject.SetActive(true);
+
+		 hordes.SetActive(false);
+		_animator.enabled = false;
+		_playerController.enabled = false;
+		
+    }
+
+
+}
